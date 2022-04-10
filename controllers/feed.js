33d5c566029d13e171702c03a78c373afd702c8a
Sourav Nag <sn.sourav1993@gmail.com>
2022-04-10 18:29:37 +0530

    Fixed a small Bug

--- controllers/feed.js
+++ controllers/feed.js
@@ -32,6 +32,29 @@ exports.getPosts = (req, res, next) => {
         });
 };
 
+// exports.getPosts = async (req, res, next) => {
+//     const currentPage = req.query.page || 1;
+//     const perPage = 2;
+//     try {
+//         const totalItems = await Post.find().countDocuments();
+//         const posts = await Post.find()
+//             .skip((currentPage - 1) * perPage)
+//             .limit(perPage);
+
+//         res.status(200).json({
+//             message: 'Fetched posts successfully.',
+//             posts: posts,
+//             totalItems: totalItems
+//         });
+//     } catch (err) {
+//         if (!err.statusCode) {
+//             err.statusCode = 500;
+//         }
+//         next(err);
+//     }
+
+// };
+
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
